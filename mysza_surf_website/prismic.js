fc3d9d834d4e398cedc263950ebdabe1897f3a1f
Patryk_Mansfeld <patryk.mansfeld@gmail.com>
2024-08-21 19:02:03 +0200

    1

--- mysza_surf_website/prismic.js
+++ mysza_surf_website/prismic.js
@@ -1,11 +1,7 @@
 import * as prismic from '@prismicio/client'
 
-// Fill in your repository name
 export const repositoryName = 'MyszaSurf'
 
 export const client = prismic.createClient(repositoryName, {
-  accessToken: 'MC5abjcyUnhjQUFDTUFzbHoy.77-977-9Gu-_ve-_ve-_ve-_ve-_ve-_ve-_ve-_vWJE77-9Eu-_vWLvv73vv73vv73vv71177-977-9fu-_vUbvv73vv73vv73vv710',
-
-  // This defines how you will structure URL paths in your project.
-  // Update the types to match the custom types in your project, and edit
+  accessToken: import.meta.env.VITE_PRISMIC_API_TOKEN,
 })
