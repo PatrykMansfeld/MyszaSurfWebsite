b0790d2dda724bff737982ac6dbfe236804e6c40
Patryk_Mansfeld <patryk.mansfeld@gmail.com>
2024-08-21 14:17:47 +0200

    Added basic RWD Layout

--- mysza_surf_website/src/components/Navbar/Navbar.jsx
+++ mysza_surf_website/src/components/Navbar/Navbar.jsx
@@ -1,39 +1,41 @@
 import Logo_Poziom from "../../assets/Logo_Poziom.jpg";
 import Instagram_Icon from "../../assets/Instagram_Icon.svg";
 import Fb_Icon from "../../assets/Fb_Icon.svg";
+import { useState } from "react";
 
-export default function Navbar() {
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="navbar-box">
       <div className="logo">
         <img className="logo_img" src={Logo_Poziom} alt="logo" />
       </div>
-      <div className="navigation">
-        <div className="navigation-socials">
-          <ul className="navigation">
-            <li className="navigation-list">
-              <a href="Instructors">Oferta</a>
-            </li>
-            <li className="navigation-list">
-              <a href="#">Zespół</a>
-            </li>
-            <li className="navigation-list">
-              <a href="#">Cennik</a>
-            </li>
-            <li className="navigation-list">
-              <a href="#">Kontakt</a>
-            </li>
-          </ul>
-          <div className="socials">
-            <a href="https://www.facebook.com/myszasurf/about" className="social-list">
-              <img className="Fb_Icon" src={Fb_Icon} alt="facebook" />
-            </a>
-            <a href="https://www.instagram.com/myszasurf?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" className="social-list">
-              <img className="Insta_Icon" src={Instagram_Icon} alt="instagram" />
-            </a>
-          </div>
+      <div className="burger-menu" onClick={toggleMenu}>
+        &#9776;
+      </div>
+      <div className={`navigation ${menuOpen ? 'open' : ''}`}>
+        <ul className="navigation-list">
+          <li><a href="Instructors">Oferta</a></li>
+          <li><a href="#">Zespół</a></li>
+          <li><a href="#">Cennik</a></li>
+          <li><a href="#">Kontakt</a></li>
+        </ul>
+        <div className="socials">
+          <a href="https://www.facebook.com/myszasurf/about" className="social-list">
+            <img className="Fb_Icon" src={Fb_Icon} alt="facebook" />
+          </a>
+          <a href="https://www.instagram.com/myszasurf?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" className="social-list">
+            <img className="Insta_Icon" src={Instagram_Icon} alt="instagram" />
+          </a>
         </div>
       </div>
-    </div>  
+    </div>
   );
-} 
+};
+
+export default Navbar;
