9434800d621a49110e8ab236396271e4ce050f73
Patryk_Mansfeld <patryk.mansfeld@gmail.com>
2024-08-26 15:44:15 +0200

    Update prismic.js

--- mysza_surf_website/prismic.js
+++ mysza_surf_website/prismic.js
@@ -3,5 +3,5 @@ import * as prismic from '@prismicio/client'
 export const repositoryName = 'MyszaSurf'
 
 export const client = prismic.createClient(repositoryName, {
-  accessToken: import.meta.env.PRISMIC_API_TOKEN,
+  accessToken: import.meta.env.VITE_PRISMIC_API_TOKEN,
 })
